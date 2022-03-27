1a35098e489295f80f1dd00dd4cdee8ebb34a334
ArcaTec <99300347+ArcaTec@users.noreply.github.com>
2022-03-26 20:39:47 -0700

    Add files via upload

--- extemporaneo3.cpp
+++ extemporaneo3.cpp
@@ -4,7 +4,7 @@
 *Capitulo 7 problema 4
 *
 *Imprimir los numeros primos del 1000 al 1500.
-* El programa no debera probar los numeros pares
+*El programa no debera probar los numeros pares
 *
 **/
 
@@ -42,12 +42,3 @@ int main() {
     return 0;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
