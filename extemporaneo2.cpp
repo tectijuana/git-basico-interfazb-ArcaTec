aeb80975cf374e1c1fb683a6fefe45a808e23144
ArcaTec <99300347+ArcaTec@users.noreply.github.com>
2022-03-27 19:35:33 -0700

    Update extemporaneo2.cpp

--- extemporaneo2.cpp
+++ extemporaneo2.cpp
@@ -5,11 +5,11 @@
 #include <cstdlib> // libreria para poder usar numeros pseudo aleatorios
 
 /**
-*Capitulo 10 problema 1
+* Capitulo 10 problema 1
 *
-*Escribir un programar que simule el lanzamiento de dos dados
+* Escribir un programar que simule el lanzamiento de dos dados
 *
-**/
+* */
 
 //@Autor    ArcaTec
 //@Fecha    26 mar 2022
@@ -28,13 +28,3 @@ int main()
     Dados();
     return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
