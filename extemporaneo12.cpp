62401a180d7cc5f187fc7d4a62cc49a8820e4edf
ArcaTec <99300347+ArcaTec@users.noreply.github.com>
2022-03-27 19:37:54 -0700

    Update extemporaneo12.cpp

--- extemporaneo12.cpp
+++ extemporaneo12.cpp
@@ -7,31 +7,31 @@
 
 
 /**
-*Capitulo 7 problema 23
+* Capitulo 7 problema 23
 *
-*En el siglo XIII, Leonardo Fibonacci, un mercader 
-*propero de Italia, a quien le fascinaban los 
-*numeros, descubiro  lo que se conocer ahora 
-*como la serie numerica de fibonacci. Cada 
-*numero de esta serie es la suma de los dos 
-*numeros que lo proceden inmediatamente. 
-*Los primeros pocos numeros de esa secuencia son: 
-*1,1,2,3,5,8,13,21,34,55,89,144,.. 
-*Encontrar los primeros treintanumeros en esta secuencia.
+* En el siglo XIII, Leonardo Fibonacci, un mercader 
+* propero de Italia, a quien le fascinaban los 
+* numeros, descubiro  lo que se conocer ahora 
+* como la serie numerica de fibonacci. Cada 
+* numero de esta serie es la suma de los dos 
+* numeros que lo proceden inmediatamente. 
+* Los primeros pocos numeros de esa secuencia son: 
+* 1,1,2,3,5,8,13,21,34,55,89,144,.. 
+* Encontrar los primeros treintanumeros en esta secuencia.
 *
-**/
+* */
 
 //@Autor    ArcaTec
 //@Fecha    27 mar 2022
 
 using namespace std;
-//Funcion de fibonacci
-int fibonacchi(int x) {
-    // asignaciones principales de los numeros donde comienza la serie
-    unsigned long long aux = 1, fib = 0, CantidadNum = x, init;
-    // ciclo donde se evalua, comienza con 0 y 1 y se repite hasta la
-    // cantidad de numeros que se quiere sacar
-    for (init = 1; init <= CantidadNum; init++) {
+
+int fibonacchi(int x) {//Funcion de fibonacci
+    
+    unsigned long long aux = 1, fib = 0, CantidadNum = x, init;// asignaciones principales de los numeros donde comienza la serie
+   
+    for (init = 1; init <= CantidadNum; init++) { // ciclo donde se evalua, comienza con 0 y 1 y se repite hasta la
+                                                  // cantidad de numeros que se quiere sacar
        
         aux += fib; /* lo mismo que aux = aux + fib; */
         fib = aux - fib;
