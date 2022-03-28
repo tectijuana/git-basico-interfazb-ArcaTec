6cf75303130dea223cab923fe7e1fa6e950a19d9
ArcaTec <99300347+ArcaTec@users.noreply.github.com>
2022-03-27 20:48:00 -0700

    Add files via upload

---
+++ extemporaneo16.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "iostream"
+#include "string"
+#include <math.h>
+#include <conio.h>
+
+
+/**
+* Capitulo 8 problema 3
+*
+* La luz puede viajar 300,000 Km por segundo.
+* Calcular la distancia que puede recorrer la luz
+* en un minuto
+*
+* */
+
+//@Autor    ArcaTec
+//@Fecha    27 mar 2022
+
+using namespace std;
+
+void Luz() {// funcion de la velocdad de la luz en un minuto
+    double rest = 300.000 * (60 );
+    cout << "\n " << rest << " Km\n";
+}
+int main()
+{
+    cout << " En un minuto la luz recorre: ";;
+    Luz();
+;
+}
